9ba684d6e6fdfdec7ec3c39653ef6a3644193abf
Esteban victorio <estebanvictorio92@gmail.com>
2019-01-25 17:34:54 -0600

    Updated start.js

--- start.js
+++ start.js
@@ -3,4 +3,3 @@ var startupSettings = require('./settings/startup');
 var resolvers = require('./utils/resolvers');
 var server = http.createServer(resolvers.requestResolver);
 server.listen(startupSettings.port,resolvers.startupResolver);
-console.log('This is on a new branch');
