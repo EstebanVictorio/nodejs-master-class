68b5cd20f6d871e92d92cc6a7a7446914567d970
Esteban victorio <estebanvictorio92@gmail.com>
2019-01-25 17:52:42 -0600

    - Renamed starting script

--- start.js
+++
@@ -1,5 +0,0 @@
-var http = require('http');
-var startupSettings = require('./settings/startup');
-var resolvers = require('./utils/resolvers');
-var server = http.createServer(resolvers.requestResolver);
-server.listen(startupSettings.port,resolvers.startupResolver);
