22240f7f459e82e3de1101d0b67cd8b60ac60644
Esteban Victorio <estebanvictorio92@gmail.com>
2019-01-26 20:00:58 -0600

    - Testing

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ var startupSettings = require('./settings/startup');
 var resolvers = require('./utils/resolvers');
 var server = http.createServer(resolvers.requestResolver);
 server.listen(startupSettings.port,resolvers.startupResolver);
+// commit comment testing
